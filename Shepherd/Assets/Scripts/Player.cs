8bad6baa16ef584cd5243d22ed493caf4ca0ca54
cartechr <54558708+cartechr@users.noreply.github.com>
2021-09-21 12:46:39 -0400

    Started player and player script

---
+++ Shepherd/Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player Speed and Jump")]
+    public float RunSpeed;
+    public float JumpSpeed;
+}
