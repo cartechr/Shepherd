9ee925167432ee64163ceacdc4625f6138c59057
cartechr <54558708+cartechr@users.noreply.github.com>
2021-10-15 10:59:19 -0400

    Coyote NavMesh

--- Shepherd/Assets/Scripts/Coyote_Script.cs
+++ Shepherd/Assets/Scripts/Coyote_Script.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Collider_Script : MonoBehaviour
+public class Coyote_Script : MonoBehaviour
 {
+    NavMeshAgent CoyoteAgent;
+    public Transform target1;
+    private void Start()
+    {
+        CoyoteAgent = GetComponent<NavMeshAgent>();
+        CoyoteAgent.destination = target1.position;
+        
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Sheep"))
+        {
+            Debug.Log("Detected Sheep");
+            CoyoteAgent.enabled = false;
+        }
+    }
 }
