fad5e2e03aa9b390d2c8d883a15ea95de4000886
cartechr <54558708+cartechr@users.noreply.github.com>
2021-10-17 17:54:24 -0400

    working on the coyote to kill the sheep

--- Shepherd/Assets/Scripts/Coyote_Script.cs
+++ Shepherd/Assets/Scripts/Coyote_Script.cs
@@ -6,7 +6,9 @@ using UnityEngine.AI;
 public class Coyote_Script : MonoBehaviour
 {
     NavMeshAgent CoyoteAgent;
+    public Transform sheep;
     public Transform target1;
+    public bool sheeptarget = false;
     private void Start()
     {
         CoyoteAgent = GetComponent<NavMeshAgent>();
@@ -19,7 +21,16 @@ public class Coyote_Script : MonoBehaviour
         if(other.gameObject.CompareTag("Sheep"))
         {
             Debug.Log("Detected Sheep");
-            CoyoteAgent.enabled = false;
+            //CoyoteAgent.enabled = false;
+            sheeptarget = true;
         }
+    }
+    private void FixedUpdate()
+    {
+        if (sheeptarget == true)
+        {
+            this.gameObject.transform.position = sheep.position;
+        }
+        sheeptarget = false;
     }
 }
--- Shepherd/Assets/Scripts/Sheep_AI.cs
+++ Shepherd/Assets/Scripts/Sheep_AI.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Sheep_AI : MonoBehaviour
 {
-    public static List<GameObject> Sheep = new List<GameObject>();
+    //public static List<GameObject> Sheep = new List<GameObject>();
+    public Coyote_Script disablesheep;
+    public GameObject sheep;
 
-    void Start()
+    private void Update()
     {
-
+        if (disablesheep.sheeptarget == true)
+        {
+            this.gameObject.SetActive(false);
+            Debug.Log("Coyote Killed Sheep");
+        }
     }
 
 }
